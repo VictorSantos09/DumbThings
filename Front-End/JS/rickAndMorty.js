984a98f7340ef3da1ed2dc4d8364c94d5829be92
Victor Santos <victorluizsantos49@gmail.com>
2022-12-25 23:53:45 -0300

    random names position

--- Front-End/JS/rickAndMorty.js
+++ Front-End/JS/rickAndMorty.js
@@ -1,16 +1,18 @@
-var rickAndMortyNameOption1 = "";
-var rickAndMortyNameOption2 = "";
-var rickAndMortyNameOption3 = "";
+var rickAndMortyNameOption1 = ""
+var rickAndMortyNameOption2 = ""
+var rickAndMortyNameOption3 = ""
 
-var correctName = "";
+var correctName = ""
+
+var resultArray = []
 
 const divSpace1 = document.getElementById("spaceNameBox-1")
 const divSpace2 = document.getElementById("spaceNameBox-2")
 const divSpace3 = document.getElementById("spaceNameBox-3")
 
-const rickAndMortyRadio1 = document.getElementById("rickAndMortyOption1");
-const rickAndMortyRadio2 = document.getElementById("rickAndMortyOption2");
-const rickAndMortyRadio3 = document.getElementById("rickAndMortyOption3");
+const rickAndMortyRadio1 = document.getElementById("rickAndMortyOption1")
+const rickAndMortyRadio2 = document.getElementById("rickAndMortyOption2")
+const rickAndMortyRadio3 = document.getElementById("rickAndMortyOption3")
 
 const GetCaracter = async function (idImg) {
 
@@ -30,22 +32,47 @@ const GetCaracter = async function (idImg) {
 
     correctName = result.correctChoice.name
 
-    document.getElementById(idImg).setAttribute("src", result.correctChoice.image);
-    document.getElementById(idImg).setAttribute("alt", "rick and morty random caracter");
+    document.getElementById(idImg).setAttribute("src", result.correctChoice.image)
+    document.getElementById(idImg).setAttribute("alt", "rick and morty random caracter")
 
-    rickAndMortyNameOption1 = document.getElementById("rickAndMortyOption-1").innerHTML = result.correctChoice.name;
-    rickAndMortyNameOption2 = document.getElementById("rickAndMortyOption-2").innerHTML = result.wrongChoice1.name;
-    rickAndMortyNameOption3 = document.getElementById("rickAndMortyOption-3").innerHTML = result.wrongChoice2.name;
+    resultArray = [result.correctChoice.name, result.wrongChoice1.name, result.wrongChoice2.name]
+
+    var randomIndex = MakeRandomIndex()
+
+    var index0 = randomIndex[0]
+    var index1 = randomIndex[1]
+    var index2 = randomIndex[2]
+
+    rickAndMortyNameOption1 = document.getElementById("rickAndMortyOption-1").innerHTML = resultArray[index0]
+    rickAndMortyNameOption2 = document.getElementById("rickAndMortyOption-2").innerHTML = resultArray[index1]
+    rickAndMortyNameOption3 = document.getElementById("rickAndMortyOption-3").innerHTML = resultArray[index2]
 }
 
 GetCaracter("rickAndMortyCaracter")
+
+function MakeRandomIndex() {
+
+    let finalArray = []
+
+    while (true) {
+        let index = Math.floor(Math.random() * 3)
+
+        if (finalArray.length == 3)
+            break
+
+        if (finalArray.includes(index) == false)
+            finalArray.push(index)
+    }
+
+    return finalArray
+}
 
 function CheckNameAndRecall(nameChoice, correctName, divId) {
     if (nameChoice == correctName)
         document.getElementById(divId).style.backgroundColor = 'green';
 
     else
-        document.getElementById(divId).style.backgroundColor = 'red';
+        document.getElementById(divId).style.backgroundColor = 'red'
 
     setTimeout(() => {
         GetCaracter("rickAndMortyCaracter")
@@ -56,9 +83,9 @@ function CleanDivsAndUncheckRadio() {
     divSpace1.style.backgroundColor = 'white'
     divSpace2.style.backgroundColor = 'white'
     divSpace3.style.backgroundColor = 'white'
-    rickAndMortyRadio1.checked = false;
-    rickAndMortyRadio2.checked = false;
-    rickAndMortyRadio3.checked = false;
+    rickAndMortyRadio1.checked = false
+    rickAndMortyRadio2.checked = false
+    rickAndMortyRadio3.checked = false
 }
 
 rickAndMortyRadio1.addEventListener("click", () => CheckNameAndRecall(rickAndMortyNameOption1, correctName, "spaceNameBox-1"))
