b776589256c47cf5ddfba28ac8461a5ed6d21b96
Victor Santos <victorluizsantos49@gmail.com>
2023-06-23 10:35:10 -0300

    inicio correção rickAndMorty

--- rickAndMorty.js
+++ rickAndMorty.js
@@ -1,15 +1,12 @@
 var rickAndMortyNameOption1 = ""
 var rickAndMortyNameOption2 = ""
 var rickAndMortyNameOption3 = ""
-
-var correctName = ""
-
+var correct = ""
 var resultArray = []
-
+const maxCaracters = 826
 const divSpace1 = document.getElementById("spaceNameBox-1")
 const divSpace2 = document.getElementById("spaceNameBox-2")
 const divSpace3 = document.getElementById("spaceNameBox-3")
-
 const rickAndMortyRadio1 = document.getElementById("rickAndMortyOption1")
 const rickAndMortyRadio2 = document.getElementById("rickAndMortyOption2")
 const rickAndMortyRadio3 = document.getElementById("rickAndMortyOption3")
@@ -17,54 +14,41 @@ const rickAndMortyRadio3 = document.getElementById("rickAndMortyOption3")
 const GetCaracter = async function (idImg) {
 
     CleanDivsAndUncheckRadio()
+    var character1 = MakeRandomIndex(maxCaracters)
+    var character2 = MakeRandomIndex(maxCaracters)
+    var character3 = MakeRandomIndex(maxCaracters)
 
-    const req = await fetch("https://localhost:7205/DumbThings/GetRickAndMorty",
+    const req = await fetch("https://rickandmortyapi.com/api/character/" + character1 + "," + character2 + "," + character3,
         {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            method: "POST",
+            method: "GET",
             body: JSON.stringify()
         })
 
     const result = await req.json();
+    console.log(result)
 
-    correctName = result.correctChoice.name
+    correct = result[0]
 
-    document.getElementById(idImg).setAttribute("src", result.correctChoice.image)
-    document.getElementById(idImg).setAttribute("alt", "rick and morty random caracter")
+    document.getElementById(idImg).setAttribute("src", correct.image)
+    document.getElementById(idImg).setAttribute("alt", "image of " + correct.name)
 
-    resultArray = [result.correctChoice.name, result.wrongChoice1.name, result.wrongChoice2.name]
+    resultArray = [result[0].name, result[1].name, result[2].name]
 
-    var randomIndex = MakeRandomIndex()
-
-    var index0 = randomIndex[0]
-    var index1 = randomIndex[1]
-    var index2 = randomIndex[2]
+    var index0 = MakeRandomIndex(3)
+    var index1 = MakeRandomIndex(3)
+    var index2 = MakeRandomIndex(3)
 
     rickAndMortyNameOption1 = document.getElementById("rickAndMortyOption-1").innerHTML = resultArray[index0]
     rickAndMortyNameOption2 = document.getElementById("rickAndMortyOption-2").innerHTML = resultArray[index1]
     rickAndMortyNameOption3 = document.getElementById("rickAndMortyOption-3").innerHTML = resultArray[index2]
 }
 
-GetCaracter("rickAndMortyCaracter")
-
-function MakeRandomIndex() {
-
-    let finalArray = []
-
-    while (true) {
-        let index = Math.floor(Math.random() * 3)
-
-        if (finalArray.length == 3)
-            break
-
-        if (finalArray.includes(index) == false)
-            finalArray.push(index)
-    }
-
-    return finalArray
+function MakeRandomIndex(max) {
+    return Math.floor(Math.random() * max)
 }
 
 function CheckNameAndRecall(nameChoice, correctName, divId) {
@@ -91,3 +75,4 @@ function CleanDivsAndUncheckRadio() {
 rickAndMortyRadio1.addEventListener("click", () => CheckNameAndRecall(rickAndMortyNameOption1, correctName, "spaceNameBox-1"))
 rickAndMortyRadio2.addEventListener("click", () => CheckNameAndRecall(rickAndMortyNameOption2, correctName, "spaceNameBox-2"))
 rickAndMortyRadio3.addEventListener("click", () => CheckNameAndRecall(rickAndMortyNameOption3, correctName, "spaceNameBox-3"))
+GetCaracter("rickAndMortyCaracter")
